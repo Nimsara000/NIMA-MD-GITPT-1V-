e813ed1895df0b504565f620f370e14153690962
Nimsara000 <ssiripala87@gmail.com>
2025-05-09 12:07:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "NIMA-MD&EOs3wCYb#ktQ5IsFy5R-0GGEIn0K6B3bZT48GtT-ykojqjAIpvTo",
+SESSION_ID: process.env.SESSION_ID || "NIMA-MD&pK9TzJRQ#yVg4U5_aqblKHWTD4SzUDLEZcOM4Bpw4RNSLwIi4ZiE",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
